5740efe5e7ba8da54a983d67f6e0e9e4a39651f8
Dean Hermanudin <dhermanu@ucsd.edu>
2016-06-09 03:13:11 -0700

    minor var changes

--- app/src/main/java/com/textme/dhermanu/githubstarred/fragments/MainActivityFragment.java
+++ app/src/main/java/com/textme/dhermanu/githubstarred/fragments/MainActivityFragment.java
@@ -136,6 +136,7 @@ public class MainActivityFragment extends Fragment {
     }
 
     private String getCurrentDate(){
+        int weekDay = 7;
         Time dayTime = new Time();
         dayTime.setToNow();
 
@@ -145,7 +146,7 @@ public class MainActivityFragment extends Fragment {
         long dateTime;
 
         // Cheating to convert this to UTC time, which is what we want anyhow
-        dateTime = dayTime.setJulianDay(julianStartDay-7);
+        dateTime = dayTime.setJulianDay(julianStartDay-weekDay);
         String day = getReadableDateString(dateTime);
         String dateFormat = "created:" + day;
 
